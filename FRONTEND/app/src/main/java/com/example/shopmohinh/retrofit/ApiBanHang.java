6e16ada26d899f7f790840979b111f5f5985b037
DecadeXD <maicanh2002@gmail.com>
2024-03-12 22:55:41 +0700

    done
    xong chức năng đăng ký

---
+++ FRONTEND/app/src/main/java/com/example/shopmohinh/retrofit/ApiBanHang.java
@@ -0,0 +1,18 @@
+package com.example.shopmohinh.retrofit;
+
+import com.example.shopmohinh.model.UserModel;
+
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ApiBanHang {
+    @POST("dangky.php")
+    @FormUrlEncoded
+    Observable<UserModel> dangKy(
+            @Field("email") String email,
+            @Field("username") String username,
+            @Field("password") String pass
+    );
+}
