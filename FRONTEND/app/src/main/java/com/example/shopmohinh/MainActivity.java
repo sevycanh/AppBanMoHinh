8f50801195821ca6e1dd61137edc740f325e78a7
Nguyễn Văn Tiến Dũng <116363146+DungIT2k2@users.noreply.github.com>
2024-03-13 14:46:26 +0700

    Create BannerQC
    Create BannerQC Main Screen

--- FRONTEND/app/src/main/java/com/example/shopmohinh/MainActivity.java
+++ FRONTEND/app/src/main/java/com/example/shopmohinh/MainActivity.java
@@ -1,14 +1,80 @@
 package com.example.shopmohinh;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.drawable.Animatable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ViewFlipper;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar toolBar;
+    ViewFlipper viewFlipper;
+    RecyclerView recyclerView;
+    NavigationView navigationView;
+    ListView listView;
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Anhxa();
+        ActionBar();
+        ActionViewFlipper();
+    }
+
+    private void ActionBar(){
+        setSupportActionBar(toolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolBar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+        toolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+
+    private void ActionViewFlipper(){
+        List<String> ArrayQuangCao = new ArrayList<>();
+        ArrayQuangCao.add("https://treobangron.com.vn/wp-content/uploads/2022/09/banner-khuyen-mai-42.jpg");
+        ArrayQuangCao.add("https://treobangron.com.vn/wp-content/uploads/2022/09/banner-khuyen-mai-40.jpg");
+        ArrayQuangCao.add("https://treobangron.com.vn/wp-content/uploads/2022/09/banner-khuyen-mai-23.jpg");
+        for (int i = 0; i < ArrayQuangCao.size(); i++){
+            ImageView imageView = new ImageView(getApplicationContext());
+            Glide.with(this).load(ArrayQuangCao.get(i)).into(imageView);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            viewFlipper.addView(imageView);
+        }
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setAutoStart(true);
+
+        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
+        Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_right);
+        viewFlipper.setInAnimation(slide_in);
+        viewFlipper.setOutAnimation(slide_out);
+    }
+
+    private void Anhxa(){
+        toolBar = findViewById(R.id.toolBarHomePage);
+        viewFlipper = findViewById(R.id.viewFlipperHomePage);
+        recyclerView = findViewById(R.id.recyclerViewHomePage);
+        navigationView = findViewById(R.id.navigationHomePage);
+        listView = findViewById(R.id.listViewHomePage);
+        drawerLayout = findViewById(R.id.drawerLayoutHomePage);
     }
 }
