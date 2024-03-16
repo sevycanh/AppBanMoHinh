af2721872631b55a5b8062e094c65e2077dc8acf
Nguyễn Văn Tiến Dũng <116363146+DungIT2k2@users.noreply.github.com>
2024-03-16 16:54:04 +0700

    Merged
    Merged completed

--- FRONTEND/app/src/main/java/com/example/shopmohinh/utils/Utils.java
+++ FRONTEND/app/src/main/java/com/example/shopmohinh/utils/Utils.java
@@ -1,5 +1,5 @@
-package com.example.shopmohinh.utils;
+package com.example.shopmohinh.Utils;
 
 public class Utils {
-    public static final String BASE_URL="http:192.168.56.1/AppBanMoHinh";
+    public static final String BASE_URL = "http://192.168.1.8/banhang/";
 }
