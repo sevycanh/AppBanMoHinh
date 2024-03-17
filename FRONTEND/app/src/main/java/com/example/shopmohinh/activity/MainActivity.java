fe133c6f78d0c210b96939f5a2e5b4f1febf6efc
VinhIT <quangvinhlai123@gmail.com>
2024-03-17 14:56:56 +0700

    update display
    activity_main.xml -> homefragment

--- FRONTEND/app/src/main/java/com/example/shopmohinh/activity/MainActivity.java
+++ FRONTEND/app/src/main/java/com/example/shopmohinh/activity/MainActivity.java
@@ -1,46 +1,50 @@
 package com.example.shopmohinh.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
-import android.graphics.drawable.Animatable;
+import android.content.Intent;
 import android.net.ConnectivityManager;
-import android.net.Network;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageView;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
-import android.widget.ViewFlipper;
 
-import com.bumptech.glide.Glide;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
-import com.denzcoskun.imageslider.interfaces.ItemChangeListener;
-import com.denzcoskun.imageslider.interfaces.ItemClickListener;
 import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.shopmohinh.R;
 import com.example.shopmohinh.adapter.SPMoiAdapter;
+import com.example.shopmohinh.fragment.AccountFragment;
+import com.example.shopmohinh.fragment.ContactFragment;
+import com.example.shopmohinh.fragment.HomeFragment;
+import com.example.shopmohinh.fragment.OrderFragment;
 import com.example.shopmohinh.model.SanPhamMoi;
 import com.example.shopmohinh.retrofit.ApiSanPhamMoi;
 import com.example.shopmohinh.retrofit.RetrofitClient;
-import com.example.shopmohinh.utils.Utils;
+
 import com.example.shopmohinh.adapter.Loaisp_Adapter;
 import com.example.shopmohinh.model.LoaiSP;
 import com.example.shopmohinh.retrofit.ApiBanHang;
-import com.example.shopmohinh.retrofit.RetrofitClient;
+
+import com.example.shopmohinh.Utils.Utils;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
-import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -61,9 +65,11 @@ public class MainActivity extends AppCompatActivity {
     CompositeDisposable compositeDisposable = new CompositeDisposable();
     ApiSanPhamMoi apiSanPhamMoi;
     ApiBanHang apiBanHang;
-    CompositeDisposable compositeDisposable = new CompositeDisposable();
     List<LoaiSP> mangLoaiSp;
     Loaisp_Adapter loaispAdapter;
+    BottomNavigationView bottomNavigationView;
+    FrameLayout frameLayout;
+    Fragment orderFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,114 +78,139 @@ public class MainActivity extends AppCompatActivity {
         apiSanPhamMoi = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiSanPhamMoi.class);
 
         Anhxa();
-        ActionBar();
-        setSearchView();
-        if (isConnected(this)){
-            Toast.makeText(getApplicationContext(), "OK!", Toast.LENGTH_LONG).show();
-            ActionViewFlipper();
+//        ActionBar();
+//        setSearchView();
+        if (isConnected(this)) {
+//            Toast.makeText(getApplicationContext(), "OK!", Toast.LENGTH_LONG).show();
+//            ActionViewFlipper();
             getSanPhamMoi();
-            getLoaiSanPham();
-        }else {
+//            getLoaiSanPham();
+        } else {
             Toast.makeText(getApplicationContext(), "Không có kết nối Internet!", Toast.LENGTH_LONG).show();
         }
-    }
-
-    private void getSanPhamMoi(){
-
-
-    }
-    private void setSearchView(){
-        searchView.setIconifiedByDefault(false);
-        searchView.setQueryHint("Tìm kiếm");
-    }
-
-    private void ActionBar() {
-        setSupportActionBar(toolBar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolBar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
-        toolBar.setNavigationOnClickListener(new View.OnClickListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                drawerLayout.openDrawer(GravityCompat.START);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.navHome) {
+                    loadFragment(new HomeFragment(), false);
+//                    loadHomePage();
+                } else if (itemId == R.id.navOrder) {
+
+                    clrFrameLayout();
+                    loadFragment(new OrderFragment(), false);
+
+                } else if (itemId == R.id.navContact) {
+                    clrFrameLayout();
+                    loadFragment(new ContactFragment(), false);
+
+                } else { // nav Account
+                    clrFrameLayout();
+                    loadFragment(new AccountFragment(), false);
+                }
+                return true;
             }
+
         });
+        loadFragment(new HomeFragment(), true);
     }
 
-    private void ActionViewFlipper() {
+    private void getSanPhamMoi() {
 
-        List<SlideModel> ArrayQuangCao = new ArrayList<>();
-        ArrayQuangCao.add(new SlideModel("https://treobangron.com.vn/wp-content/uploads/2022/09/banner-khuyen-mai-42.jpg", null));
-        ArrayQuangCao.add(new SlideModel("https://treobangron.com.vn/wp-content/uploads/2022/09/banner-khuyen-mai-40.jpg", null));
-        ArrayQuangCao.add(new SlideModel("https://treobangron.com.vn/wp-content/uploads/2022/09/banner-khuyen-mai-23.jpg", null));
-        imageSlider.setImageList(ArrayQuangCao, ScaleTypes.CENTER_CROP);
 
-        imageSlider.setOnTouchListener(touchListener);
     }
 
-    View.OnTouchListener touchListener = new View.OnTouchListener() {
-        private float startX;
-        private float startY;
-        private long startTime;
+//    private void setSearchView() {
+//        searchView.setIconifiedByDefault(false);
+//        searchView.setQueryHint("Tìm kiếm");
+//    }
 
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    startX = event.getX();
-                    startY = event.getY();
-                    startTime = System.currentTimeMillis();
-                    break;
-                case MotionEvent.ACTION_UP:
-                    float endX = event.getX();
-                    float endY = event.getY();
-                    long endTime = System.currentTimeMillis();
+//    private void ActionBar() {
+//        setSupportActionBar(toolBar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        toolBar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
+//        toolBar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                drawerLayout.openDrawer(GravityCompat.START);
+//            }
+//        });
+//    }
 
-                    float deltaX = endX - startX;
-                    float deltaY = endY - startY;
-                    long deltaTime = endTime - startTime;
+//    private void ActionViewFlipper() {
+//
+//        List<SlideModel> ArrayQuangCao = new ArrayList<>();
+//        ArrayQuangCao.add(new SlideModel("https://treobangron.com.vn/wp-content/uploads/2022/09/banner-khuyen-mai-42.jpg", null));
+//        ArrayQuangCao.add(new SlideModel("https://treobangron.com.vn/wp-content/uploads/2022/09/banner-khuyen-mai-40.jpg", null));
+//        ArrayQuangCao.add(new SlideModel("https://treobangron.com.vn/wp-content/uploads/2022/09/banner-khuyen-mai-23.jpg", null));
+//        imageSlider.setImageList(ArrayQuangCao, ScaleTypes.CENTER_CROP);
+//
+//        imageSlider.setOnTouchListener(touchListener);
+//    }
 
-                    // Tính toán khoảng cách và thời gian để xác định hướng di chuyển
-                    if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > 100 && deltaTime < 200) {
-                        // Xác định người dùng đã kéo sang trái hay sang phải
-                        if (deltaX > 0) {
-                            // Kéo sang phải
-                            imageSlider.startSliding(10000);
-                        } else {
-                            // Kéo sang trái
-                            imageSlider.startSliding(10000);
-                        }
-                    }
-                    break;
-            }
-            return true;
-        }
-        // retrofit get data tbl_category
-
-    };
+//    View.OnTouchListener touchListener = new View.OnTouchListener() {
+//        private float startX;
+//        private float startY;
+//        private long startTime;
+//
+//        @Override
+//        public boolean onTouch(View v, MotionEvent event) {
+//            switch (event.getAction()) {
+//                case MotionEvent.ACTION_DOWN:
+//                    startX = event.getX();
+//                    startY = event.getY();
+//                    startTime = System.currentTimeMillis();
+//                    break;
+//                case MotionEvent.ACTION_UP:
+//                    float endX = event.getX();
+//                    float endY = event.getY();
+//                    long endTime = System.currentTimeMillis();
+//
+//                    float deltaX = endX - startX;
+//                    float deltaY = endY - startY;
+//                    long deltaTime = endTime - startTime;
+//
+//                    // Tính toán khoảng cách và thời gian để xác định hướng di chuyển
+//                    if (Math.abs(deltaX) > Math.abs(deltaY) && Math.abs(deltaX) > 100 && deltaTime < 200) {
+//                        // Xác định người dùng đã kéo sang trái hay sang phải
+//                        if (deltaX > 0) {
+//                            // Kéo sang phải
+//                            imageSlider.startSliding(10000);
+//                        } else {
+//                            // Kéo sang trái
+//                            imageSlider.startSliding(10000);
+//                        }
+//                    }
+//                    break;
+//            }
+//            return true;
+//        }
+//
+//
+//    };
 
     private void Anhxa() {
-        toolBar = findViewById(R.id.toolBarHomePage);
-        recyclerView = findViewById(R.id.recyclerViewHomePage);
-        navigationView = findViewById(R.id.navigationHomePage);
-        listView = findViewById(R.id.listViewHomePage);
-        drawerLayout = findViewById(R.id.drawerLayoutHomePage);
-        imageSlider = findViewById(R.id.imageSliderHomePage);
-        searchView = findViewById(R.id.searchHomePage);
+//        toolBar = findViewById(R.id.toolBarHomePage);
+//        recyclerView = findViewById(R.id.recyclerViewHomePage);
+//        navigationView = findViewById(R.id.navigationHomePage);
+//        listView = findViewById(R.id.listViewHomePage);
+//        drawerLayout = findViewById(R.id.drawerLayoutHomePage);
+//        imageSlider = findViewById(R.id.imageSliderHomePage);
+//        searchView = findViewById(R.id.searchHomePage);
         //Khoi tao list
-        mangSanPhamMoi = new ArrayList<>();
+//        mangSanPhamMoi = new ArrayList<>();
 
         //Khoi tao Adapter
-        spMoiAdapter = new SPMoiAdapter(getApplicationContext(), mangSanPhamMoi);
-        listView.setAdapter(spMoiAdapter);
+//        spMoiAdapter = new SPMoiAdapter(getApplicationContext(), mangSanPhamMoi);
+//        listView.setAdapter(spMoiAdapter);
 
         // retrofit get data tbl_category
-        mangLoaiSp = new ArrayList<>();
-        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+//        mangLoaiSp = new ArrayList<>();
+//        apiBanHang = RetrofitClient.getInstance(Utils.BASE_URL).create(ApiBanHang.class);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
 
     }
 
-
-    }
 
     private boolean isConnected(Context context) {
         ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -192,18 +223,37 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void getLoaiSanPham() {
-        compositeDisposable.add(apiBanHang.getLoaiSp().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
-                .subscribe(
-                        loaiSPModel -> {
-                            if (loaiSPModel.isSuccess()) {
-//                                Toast.makeText(getApplicationContext(),loaiSPModel.getResult().get(0).getName(), Toast.LENGTH_LONG).show();
-                                mangLoaiSp = loaiSPModel.getResult();
-                                loaispAdapter = new Loaisp_Adapter(getApplicationContext(), mangLoaiSp);
-                                ListView listView = findViewById(R.id.listViewHomePage);
-                                listView.setAdapter(loaispAdapter);
-                            }
-                        }
-                ));
+//    private void getLoaiSanPham() {
+//        compositeDisposable.add(apiBanHang.getLoaiSp().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(
+//                        loaiSPModel -> {
+//                            if (loaiSPModel.isSuccess()) {
+////                                Toast.makeText(getApplicationContext(),loaiSPModel.getResult().get(0).getName(), Toast.LENGTH_LONG).show();
+//                                mangLoaiSp = loaiSPModel.getResult();
+//                                loaispAdapter = new Loaisp_Adapter(getApplicationContext(), mangLoaiSp);
+//                                ListView listView = findViewById(R.id.listViewHomePage);
+//                                listView.setAdapter(loaispAdapter);
+//                            }
+//                        }
+//                ));
+//    }
+
+    private void loadFragment(Fragment fragment, boolean isAppInitialized) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (isAppInitialized) {
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+//            Toast.makeText(this, "Fragment", Toast.LENGTH_SHORT).show();
+
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+        fragmentTransaction.commit();
     }
+
+    private void clrFrameLayout() {
+        frameLayout = findViewById(R.id.frameLayout);
+        frameLayout.removeAllViews();
+    }
+
 }
