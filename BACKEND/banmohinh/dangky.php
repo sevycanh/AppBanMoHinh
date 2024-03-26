a5b77993b8ff31e008c7603ac3e3780b211ecdc4
DecadeXD <maicanh2002@gmail.com>
2024-03-27 03:10:11 +0700

    update backend develop

--- BACKEND/banmohinh/dangky.php
+++ BACKEND/banmohinh/dangky.php
@@ -1,7 +1,7 @@
 <?php
 include "connect.php";
 $email=$_POST['email'];
-$pass=$_POST['password'];
+// $pass=$_POST['password'];
 
 //checkdata
 $query = 'SELECT * FROM `tbl_account` WHERE `email` = "'.$email.'"';
@@ -14,7 +14,7 @@ if ($numrow > 0){
 	];
 } else {
 	//insert
-	$query = 'INSERT INTO `tbl_account`(`email`, `password`, `role`, `status`) VALUES ("'.$email.'","'.$pass.'", "1", "1")';
+	$query = 'INSERT INTO `tbl_account`(`email`, `role`, `status`) VALUES ("'.$email.'", "1", "1")';
 	$data = mysqli_query($conn, $query);
 
 	if ($data == true){
--- BACKEND/banmohinh/dangnhap.php
+++ BACKEND/banmohinh/dangnhap.php
@@ -1,10 +1,10 @@
 <?php
 include "connect.php";
 $email=$_POST['email'];
-$pass=$_POST['pass'];
+// $pass=$_POST['pass'];
 
 //checkdata
-$query = 'SELECT * FROM `tbl_account` WHERE `email` = "'.$email.'" AND `password` ="'.$pass.'"';
+$query = 'SELECT * FROM `tbl_account` WHERE `email` = "'.$email.'"';
 $data = mysqli_query($conn, $query);
 $result = array();
 while ($row = mysqli_fetch_assoc($data)) {
