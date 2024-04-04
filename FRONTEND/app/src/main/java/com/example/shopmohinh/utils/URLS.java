7d1e13bd8293d4567ef103771413986d1a2a3d95
DecadeXD <maicanh2002@gmail.com>
2024-04-04 14:20:55 +0700

    done feature/signin_google

--- FRONTEND/app/src/main/java/com/example/shopmohinh/utils/URLS.java
+++ FRONTEND/app/src/main/java/com/example/shopmohinh/utils/URLS.java
@@ -1,6 +1,6 @@
 package com.example.shopmohinh.utils;
 
 public class URLS {
-    public static final String ip = "http://192.168.1.8:8019/banmohinh/";
+    public static final String ip = "http://192.168.43.114:8019/banmohinh/";
 
 }
